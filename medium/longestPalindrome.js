b7b7821320bdc5b857b15df6cfb47217545eff5e
S1nBer <gerasimenko_andrei@bk.ru>
2026-07-28 15:38:13 +0700

    Add longestPalindrome

---
+++ medium/longestPalindrome.js
@@ -0,0 +1,46 @@
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+
+// Example 2:
+// Input: s = "cbbd"
+// Output: "bb"
+ 
+// Constraints:
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+function longestPalindrome(s) {
+    if (s.length === 1) return s;
+    
+    let start = 0, maxLength = 1;
+    
+    for (let i = 0; i < s.length; i++) {
+        // Нечетный палиндром (центр = i)
+        expandAroundCenter(i, i);
+        // Четный палиндром (центр между i и i+1)
+        expandAroundCenter(i, i + 1);
+    }
+    
+    function expandAroundCenter(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            const currentLength = right - left + 1;
+            if (currentLength > maxLength) {
+                start = left;
+                maxLength = currentLength;
+            }
+            left--;
+            right++;
+        }
+    }
+    
+    return s.slice(start, start + maxLength);
+}
+
+console.log(longestPalindrome("f"));
+console.log(longestPalindrome("babad"));
+console.log(longestPalindrome("cbbd"));
+console.log(longestPalindrome("uwqrvqslistiezghcxaocjbhtktayupazvowjrgexqobeymperyxtfkchujjkeefmdngfabycqzlslocjqipkszmihaarekosdkwvsirzxpauzqgnftcuflzyqwftwdeizwjhloqwkhevfovqwyvwcrosexhflkcudycvuelvvqlbzxoajisqgwgzhioomucfmkmyaqufqggimzpvggdohgxheielsqucemxrkmmagozxhvxlwvtbbcegkvvdrgkqszgajebbobxnossfrafglxvryhvyfcibfkgpbsorqprfujfgbmbctsenvbzcvypcjubsnjrjvyznbswqawodghmigdwgijfytxbgpxreyevuprpztmjejkaqyhppchuuytkdsteroptkouuvmkvejfunmawyuezxvxlrjulzdikvhgxajohpzrshrnngesarimyopgqydcmsaciegqlpqnclpwcjqmhtmtwwtbkmtnntdllqbyyhfxsjyhugnjbebtxeljytoxvqvrxygmtogndrhlcmbmgiueliyfkkcuypvvzkomjrfhuhhnfbxeuvssvvllgukdolffukzwqaimxkngnjnmsbvwkajyxqntsqjkjqvwxnlxwjfiaofejtjcveqstqhdzgqistxwsgrovvwgorjaoosremqbzllgbgrwtqdggxnyvkivlcvnv"));
